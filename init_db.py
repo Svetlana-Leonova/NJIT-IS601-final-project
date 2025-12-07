d9c5a75f051a7c87e4f70ea09c9b5ca257b20b04
Svetlana Leonova <leonova_svetlana@yahoo.com>
2025-12-07 18:53:26 -0500

    cleanup init_db

--- init_db.py
+++ init_db.py
@@ -1,9 +1,6 @@
 import sqlite3
 import json
 
-# Helper functions
-
-# Initialize the database from example-orders.json
 def init_db():
     # Open a connection to the database
     connection = sqlite3.connect("dosa.db")
@@ -58,22 +55,6 @@ def init_db():
         cursor.execute("INSERT INTO item_list (order_id, item_id) VALUES (?, ?);",
                        (order_id, item_id))
 
-    def list_customers():
-        rows = cursor.execute("SELECT id, name, phone FROM customers;").fetchall()
-        return rows
-
-    def list_items():
-        rows = cursor.execute("SELECT id, name, price FROM items;").fetchall()
-        return rows
-
-    def list_orders():
-        rows = cursor.execute("SELECT id, timestamp, cust_id, notes FROM orders;").fetchall()
-        return rows
-
-    def list_item_list():
-        rows = cursor.execute("SELECT order_id, item_id FROM item_list;").fetchall()
-        return rows
-
     def count_customers():
         rows = cursor.execute("SELECT COUNT(*) FROM customers;").fetchone()
         return rows[0]
@@ -90,24 +71,24 @@ def init_db():
         rows = cursor.execute("SELECT COUNT(*) FROM item_list;").fetchone()
         return rows[0]
 
-
-    # Load data from example-orders.json
-    with open("./data/example_orders.json", "r") as f:
-        orders = json.load(f)
-
     # Insert data into tables
     # Seed customers
     with open("./data/customers.json", "r") as f:
         customers = json.load(f)
     for phone, name in customers.items():
         add_customer(name, phone)
+    print(f"Added {count_customers()} customers to dosa.db")
 
     # Seed items
     with open("./data/items.json", "r") as f:
         items = json.load(f)
     for name, item in items.items():
         add_item(name, item["price"])
+    print(f"Added {count_items()} items to dosa.db")
 
+    # Load data from example-orders.json
+    with open("./data/example_orders.json", "r") as f:
+        orders = json.load(f)
     # Seed orders and item list
     for order in orders:
         timestamp = order["timestamp"]
@@ -117,28 +98,17 @@ def init_db():
         cust_id = cursor.execute("SELECT id FROM customers WHERE phone = ?;", (phone,)).fetchone()[0]
         add_order(timestamp, cust_id, notes)
         order_id = cursor.lastrowid
+        items = cursor.execute("SELECT id, name, price FROM items;").fetchall()
         for item in order["items"]:
-            if item["name"] not in list_items():
+            if item["name"] not in [item[1] for item in items]:
                 add_item(item["name"], item["price"])
                 item_id = cursor.lastrowid
                 add_item_list(order_id, item_id)
             else:
                 item_id = cursor.execute("SELECT id FROM items WHERE name = ?;", (item["name"],)).fetchone()[0]
                 add_item_list(order_id, item_id)
-
-    # Print the data
-    # print(list_customers())
-    # print(list_items())
-    # print(list_orders())
-    # print(list_item_list())
-    print("Customers:")
-    print(count_customers())
-    print("Items:")
-    print(count_items())
-    print("Orders:")
-    print(count_orders())
-    print("Item List:")
-    print(count_item_list())
+    print(f"Added {count_orders()} orders to dosa.db")
+    print(f"Added {count_item_list()} item list entries to dosa.db")
 
     # Commit the changes and close the connection
     connection.commit()
